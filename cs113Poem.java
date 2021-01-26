8e3f7a3acb5d18faa71ee5b04ab23c1ecfd07c01
jschaedler <jschaedler7@gmail.com>
2021-01-26 11:54:11 -0500

    Poem Output

---
+++ cs113Poem.java
@@ -0,0 +1,10 @@
+public class cs113Poem {
+    public static void main(String[] args) {
+        System.out.println("Roses are red,");
+        System.out.println("violets are blue,");
+        System.out.println("sugar is sweet,");
+        System.out.println("and so are you.");
+
+    }
+
+}
