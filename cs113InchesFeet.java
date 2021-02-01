900ed46f9ebf24256194caabbabe102d4d448617
jschaedler <jschaedler7@gmail.com>
2021-01-31 20:13:38 -0500

    Finished

--- cs113InchesFeet.java
+++ cs113InchesFeet.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class cs113InchesFeet {
-    private static Scanner scan;
 
     public static void main(String[] args) {
         int inches;
         int feet;
-        scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter Number of Inches");
         inches = scan.nextInt();
         feet = inches / 12;
         System.out.println("Feet: " + feet);
 
+        scan.close();
     }
 }
