e323a2ed2ffbb1721c77345a80d0661c0cbc0fcb
jschaedler <jschaedler7@gmail.com>
2021-02-06 19:30:02 -0500

    Username Generator

---
+++ NameApp.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class NameApp {
+    public static void main(String[] args)
+
+    {
+        String first, last;
+        int number;
+        Scanner scan = new Scanner(System.in);
+        Random num = new Random();
+        System.out.print("Enter your first name: ");
+        first = scan.nextLine();
+
+        System.out.print("Enter your last name: ");
+        last = scan.nextLine();
+
+        number = num.nextInt(98) + 1;
+
+        System.out.print(last.substring(0, 1) + first.substring(0, 5) + number);
+
+        scan.close();
+
+    }
+}
