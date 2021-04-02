1b2cbbdef8b2d663c66bbb6bc2dcf0ca0d41f29a
jschaedler <jschaedler7@gmail.com>
2021-04-02 15:08:48 -0400

    Hw question 2/3

--- testDice.java
+++ testDice.java
@@ -1,16 +1,25 @@
 
-public class testDice {
+public class TestDice {
 
     public static void main(String[] args) {
+        Die tempDie = new Die();
+        System.out.println(tempDie);
+        Die[] diceArray = new Die[50];
+        for (int i = 0; i < 50; i++) {
+            tempDie.roll();
+            diceArray[i] = tempDie;
+        }
 
-        Die die1 = new Die();
-        Die die2 = new Die();
-        System.out.println(die1 + "\n" + die2);
-        die1.roll();
-        die2.roll();
+        int count = 0;
+        for (Die d : diceArray) {
+            d.roll();
+            int value = d.getFaceValue();
+            if (value % 2 == 0) {
+                count++;
+            }
 
-        System.out.println(die1 + "\n" + die2);
-        die2.setFaceValue(3);
-        System.out.println(die2);
+        }
+
+        System.out.println("The number of die that have an even face value is " + count);
     }
 }
