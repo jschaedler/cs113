b82940769d7cbe6c9a9769baca6547641146c7d1
jschaedler <jschaedler7@gmail.com>
2021-04-11 19:35:13 -0400

    Test 1 Question 3

---
+++ Practice2C.java
@@ -0,0 +1,11 @@
+public class Practice2C {
+
+    public int topPlayers(Player[] team, int target) {
+        int count = 0;
+        for (int i = 0; i < team.length; i++) {
+            if (team[i].getAverage > target)
+                count++;
+        }
+    }
+
+}
