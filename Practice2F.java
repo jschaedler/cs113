f458cc9548114d239188e31588299c2a33249df2
jschaedler <jschaedler7@gmail.com>
2021-04-11 22:41:17 -0400

    Test 2 Question 3

---
+++ Practice2F.java
@@ -0,0 +1,16 @@
+public class Practice2F {
+    public double[] bestInMonths(double[][] famCellBills) {
+        int col = famCellBills.length;
+        double[] minMonthly = new double[col];
+        double minPrice = Double.MAX_VALUE;
+        for (int i = 0; i < col; i++) {
+            for (int row = 0; row < famCellBills[col].length; row++) {
+                if (famCellBills[row][i] < minPrice)
+                    minPrice = famCellBills[row][i];
+            }
+            minMonthly[i] = minPrice;
+        }
+
+        return minMonthly;
+    }
+}
