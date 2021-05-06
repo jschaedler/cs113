5d10912e48e60ccc32dd3c3665ec795caafbbea4
jschaedler <jschaedler7@gmail.com>
2021-05-06 11:41:31 -0400

    commented final pratice

--- Circle.java
+++ Circle.java
@@ -1,4 +1,19 @@
 public class Circle {
+
+    /*
+     * protected double radius;
+     * 
+     * public Circle(double r) { radius = r; }
+     * 
+     * public void setRadius(double r) { radius = r; }
+     * 
+     * public double getRadius() { return radius; }
+     * 
+     * public double area() { return Math.PI * radius * radius; }
+     * 
+     * public String toString() { return "Circle:Radius= " + radius; } }
+     */
+
     private Point center;
     private float radius;
 
@@ -29,5 +44,4 @@ public class Circle {
         String info = "The point has x and y values: " + "\n" + center + "\nThe radius is: " + radius;
         return info;
     }
-
 }
